11d458c4b0e1a86aa76a0647d22281441b24450b
Gen <hugogiman17@gmail.com>
2021-06-25 16:19:00 +0700

    update custerr

--- custerr/custerr.go
+++ custerr/custerr.go
@@ -10,38 +10,38 @@ import (
 )
 
 func CustomError(err error) error {
-	customErr := errors.New("Gagal: ")
+	var customErr []string
 	if castedObject, ok := err.(validator.ValidationErrors); ok {
 		for _, err := range castedObject {
 			switch err.Tag() {
 			case "required":
-				customErr = fmt.Errorf("%w"+fmt.Sprintf("%s wajib diisi, ", err.Field()), customErr)
+				customErr = append(customErr, fmt.Sprintf("%s wajib diisi, ", err.Field()))
 			case "email":
-				customErr = fmt.Errorf("%w"+fmt.Sprintf("format %s tidak valid, ", err.Field()), customErr)
+				customErr = append(customErr, fmt.Sprintf("format %s tidak valid,", err.Field()))
 			}
 		}
 	} else if code, ok := err.(*mysql.MySQLError); ok {
 		switch code.Number {
 		case 1062:
 			if strings.Contains(code.Message, "email") {
-				customErr = fmt.Errorf("%wEmail sudah terpakai ", customErr)
+				customErr = append(customErr, fmt.Sprintf("Email sudah terpakai"))
 			} else if strings.Contains(code.Message, "username") {
-				customErr = fmt.Errorf("%wUsername sudah terpakai ", customErr)
+				customErr = append(customErr, fmt.Sprintf("Username sudah terpakai"))
 			} else if strings.Contains(code.Message, "slug") {
-				customErr = fmt.Errorf("%wDomain sudah terpakai ", customErr)
+				customErr = append(customErr, fmt.Sprintf("Domain sudah terpakai"))
 			} else {
-				customErr = fmt.Errorf("%wNama sudah terpakai ", customErr)
+				customErr = append(customErr, fmt.Sprintf("Nama sudah terpakai"))
 			}
 		case 1264:
-			customErr = fmt.Errorf("%wJumlah karakter terlalu panjang", customErr)
+			customErr = append(customErr, fmt.Sprintf("Jumlah karakter terlalu panjang"))
 		case 1406:
-			customErr = fmt.Errorf("%wJumlah karakter terlalu panjang", customErr)
+			customErr = append(customErr, fmt.Sprintf("Jumlah karakter terlalu panjang"))
 		default:
-			customErr = err
+			customErr = append(customErr, fmt.Sprintf(err.Error()))
 		}
 	} else {
-		customErr = err
+		customErr = append(customErr, fmt.Sprintf(err.Error()))
 	}
 
-	return customErr
+	return errors.New(strings.Join(customErr, "\n"))
 }
